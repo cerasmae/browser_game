37e3fea43fea2962cef2bad3e41c6740bab862a5
Japheth Balane <japheth162@gmail.com>
2019-10-17 15:44:21 +0800

    Fixed balls label position code cleaning

--- assets/js/game.js
+++ assets/js/game.js
@@ -28,8 +28,8 @@ var beginGame = 0;
 var totalBalls = 20;
 var totalColors = totalBalls / 5;
 
-var mouseX = canvas.width/2;
-var mouseY = canvas.height/2;
+var mouseX = canvas.width / 2;
+var mouseY = canvas.height / 2;
 var clickX;
 var clickY;
 
@@ -52,86 +52,74 @@ var wingsAudio = new Howl({
 ///////////////////////////////////////////////////////////////////////
 
 function clearCanvas() {
-  context.fillStyle = bckgrColor;
-  context.fillRect(0,0,canvas.width,canvas.height);
+    context.fillStyle = bckgrColor;
+    context.fillRect(0, 0, canvas.width, canvas.height);
 }
 
 function randomBetween(min,max) {
-  return Math.floor((Math.random()*(max - min)+min));
+    return Math.floor((Math.random() * (max - min) + min));
 }
 
 function getHypothenuse(x1, y1, x2, y2) {
-  var diffx = Math.abs(x1 - x2);
-  var diffy = Math.abs(y1 - y2);
-  return Math.sqrt(diffx*diffx+diffy*diffy);
+    var diffx = Math.abs(x1 - x2);
+    var diffy = Math.abs(y1 - y2);
+    return Math.sqrt(diffx * diffx + diffy * diffy);
 }
 
-function colorChooser(){
+function colorChooser() {
     var currColor;
     var isIn = false;
-    if(selectedColors.length < 1){
+    if (selectedColors.length < 1) {
         currColor = colors[randomBetween(0, colors.length)];
         selectedColors.push(currColor);
         return currColor;
-    } else if( selectedColors.length == balls.length/5 ){
+    } else if (selectedColors.length == balls.length / 5 ) {
         return colors[randomBetween(0, colors.length)];
-    } else{
+    } else {
         //assures that all colors will be included
-        for(var i = 0; i < colors.length; i++){
-
-            if( selectedColors.indexOf(colors[i]) == -1 ){
+        for (var i = 0; i < colors.length; i++) {
+            if (selectedColors.indexOf(colors[i]) == -1) {
                 currColor = colors[i];
                 selectedColors.push(currColor);
                 console.log('color: ', currColor, selectedColors.length);
                 return currColor;
             }
         }
-
         return colors[randomBetween(0, colors.length)];
     }
 }
 
-
 ///////////////////////////////////////////////////////////////////
 
-function initializeGame(){
+function initializeGame() {
     balls = [];
     colors = [];
     selectedColors = [];
-
-    for( var i = 0; i < totalColors; i++){
+    for (var i = 0; i < totalColors; i++) {
         colors[i] = randomColor();
     }
-
     console.log('allColors: ', colors, colors.length);
-
     characterColor = colors[randomBetween(0, colors.length)];
     characterRadius = 10;
     score = 0;
-
-    for( var i = 0; i < totalBalls; i++ ){
+    for (var i = 0; i < totalBalls; i++) {
         balls.push(new Ball());
     }
-
 }
 
-function leveling(totBalls, totColors){
-
-    for( var i = 0; i < totColors; i++){
+function leveling(totBalls, totColors) {
+    for(var i = 0; i < totColors; i++) {
         colors[i] = randomColor();
     }
-
-    for( var i = 0; i < totBalls; i++ ){
+    for (var i = 0; i < totBalls; i++) {
         balls.push(new Ball());
     }
-
 }
 
-function initializeMenu(){
+function initializeMenu() {
     menuBalls[0] = new playBall();
-    menuBalls[1] = new rotatingBalls(80)//(randomBetween(0, 180));
-    menuBalls[2] = new rotatingBalls(146)//(randomBetween(0, 180));
-
+    menuBalls[1] = new rotatingBalls(80) //(randomBetween(0, 180));
+    menuBalls[2] = new rotatingBalls(146) //(randomBetween(0, 180));
     characterColor = colors[randomBetween(0, colors.length)];
     characterRadius = 10;
     score = 0;
@@ -139,59 +127,53 @@ function initializeMenu(){
 
 function Ball() {
     this.radius = randomBetween(10, 50);
-    this.x = randomBetween(0+this.radius, canvas.width-this.radius);
-    this.y = randomBetween(0+this.radius, canvas.height-this.radius);
+    this.x = randomBetween(0 + this.radius, canvas.width-this.radius);
+    this.y = randomBetween(0 + this.radius, canvas.height-this.radius);
     this.inRangeX = false;
     this.inRangeY = false;
     this.repel = false;
 
     this.color = colorChooser();
 
-    this.dirx = randomBetween(-5,6);
-    this.diry = randomBetween(-5,6);
+    this.dirx = randomBetween(-5, 6);
+    this.diry = randomBetween(-5, 6);
 
-    if( this.dirx == 0 ){
+    if (this.dirx == 0) {
         this.dirx = 1;
     }
 
-    if( this.diry == 0 ){
+    if (this.diry == 0 ) {
         this.diry = 1;
     }
 
     this.update = function() {
         this.x += this.dirx;
         this.y += this.diry;
-
-        if (  this.x - this.radius <= 0 ||
-            this.x + this.radius >= canvas.width) {
+        if (this.x - this.radius <= 0 || this.x + this.radius >= canvas.width) {
             this.dirx *= -1;
         }
-        if (  this.y - this.radius <= 0 ||
-            this.y + this.radius >= canvas.height) {
+        if (this.y - this.radius <= 0 || this.y + this.radius >= canvas.height) {
             this.diry *= -1;
         }
-
         return this;
-        }
+    }
 
     this.draw = function() {
-
         context.fillStyle = this.color;
         context.beginPath();
-        context.arc(this.x, this.y, this.radius, Math.PI*2, false);
+        context.arc(this.x, this.y, this.radius, Math.PI * 2, false);
         context.fill();
-
     }
 }
 
-function playBall(){
+function playBall() {
     this.radius = 100;
-    this.x = canvas.width/2;
-    this.y = canvas.height/2;
+    this.x = canvas.width / 2;
+    this.y = canvas.height / 2;
 
     this.color = randomColor();
 
-    this.update = function(){
+    this.update = function() {
         return this;
     }
 
@@ -202,24 +184,24 @@ function playBall(){
         context.arc(this.x, this.y, this.radius, Math.PI*2, false);
         context.fill();
 
+        context.textBaseline = 'middle';
         context.fillStyle = '#000';
-        context.fillText(name, this.x-15, this.y+5);
-
+        context.fillText(name, this.x - context.measureText(name).width / 2, this.y);
     }
 }
 
-function rotatingBalls(point){
+function rotatingBalls(point) {
     this.radius = 50;
     this.path = 200;
-    this.origx = canvas.width/2;
-    this.origy = canvas.height/2;
-    this.x = canvas.width/2;
-    this.y = canvas.height/2;
+    this.origx = canvas.width / 2;
+    this.origy = canvas.height / 2;
+    this.x = canvas.width / 2;
+    this.y = canvas.height / 2;
     this.point = point;
 
     this.color = randomColor();
 
-    this.update = function(){
+    this.update = function() {
         this.point += 0.03;
 
         this.x = Math.floor(this.origx + (this.path * Math.cos(this.point)));
@@ -229,137 +211,122 @@ function rotatingBalls(point){
     }
 
     this.draw = function(name) {
-
         context.fillStyle = this.color;
         context.beginPath();
         context.arc(this.x, this.y, this.radius, Math.PI*2, false);
         context.fill();
 
+        context.textBaseline = 'middle';
         context.fillStyle = '#000';
-        context.fillText(name, this.x - name.length*4, this.y + name.length*1.7);
-
+        context.fillText(name, this.x - context.measureText(name).width / 2, this.y);
     }
 }
 
 function drawCharacter() {
-      context.fillStyle = characterColor;//'#000';
+      context.fillStyle = characterColor; //'#000';
       context.beginPath();
-      context.arc(mouseX, mouseY, characterRadius, Math.PI*2, false);
+      context.arc(mouseX, mouseY, characterRadius, Math.PI * 2, false);
       context.fill();
 }
-
 
 //////////////////////////////////////////////////////////////////////////
 
 function world() {
 
     var nameBall;
-    console.log('showhow', showHow);
     clearCanvas();
 
-    switch(beginGame){
+    switch (beginGame) {
         case 0: //menu
-            if(showHow){
+            if (showHow) {
                 context.fillStyle = fontColor;
                 context.fillText('Collide with balls of your same color.', 50, 100);
                 context.fillText('Be careful though! You don\'t want to.', 50, 125);
                 context.fillText('mess with the other colors. :>', 50, 150);
             }
 
-            for( var i = 0; i < menuBalls.length; i++){
-                if( i == 0 ){
+            for (var i = 0; i < menuBalls.length; i++) {
+                if (i == 0) {
                     nameBall = 'Play';
-                } else if( i == 1 ){
+                } else if(i == 1) {
                     nameBall = 'How';
-                } else{
+                } else {
                     nameBall = 'Lights';
                 }
                 menuBalls[i].update().draw(nameBall);
             }
-
             drawCharacter();
-
-        break;
+            break;
 
     case 1: //game
-
         for (var i = 0; i < balls.length; i++) {
-            if(getHypothenuse(mouseX, mouseY, balls[i].x, balls[i].y ) <=
-                characterRadius + balls[i].radius && mouseMoved ){
+            if (getHypothenuse(mouseX, mouseY, balls[i].x, balls[i].y ) <= characterRadius + balls[i].radius && mouseMoved) {
                 console.log(characterColor, balls[i].color);
-                if( balls[i].color == characterColor ){
+                if (balls[i].color == characterColor) {
                     score+=balls[i].radius;
-                    selectedColors.splice( selectedColors.indexOf(balls[i].color), 1 );
+                    selectedColors.splice(selectedColors.indexOf(balls[i].color), 1);
                     balls[i] = new Ball();
                     characterColor = selectedColors[randomBetween(0, selectedColors.length)];
                     sameScore = false;
                     sameCounter++;
-                } else{
+                } else {
                     balls[i].repel = true;
                     score -= 2;
                     sameScore = false;
                     characterRadius *= 1.01;
                     otherCounter--;
                 }
-            } else{
+            } else {
                 balls[i].repel = false;
             }
-            if( balls[i].repel ){
+            if (balls[i].repel) {
                 balls[i].dirx *= -1;
                 balls[i].diry *= -1;
                 balls[i].repel = false;
             }
             balls[i].update().draw();
         }
-
-
-        if( sameCounter % 10 == 0 && characterRadius > 20 && sameCounter != 0){
+        if (sameCounter % 10 == 0 && characterRadius > 20 && sameCounter != 0) {
             characterRadius -= 5;
             sameCounter++;
-        } else if( otherCounter % 25 == 0 && !sameScore && otherCounter != 0 ){
+        } else if ( otherCounter % 25 == 0 && !sameScore && otherCounter != 0) {
             sameScore = true;
             characterRadius += 1;
         }
-
-        if(sameCounter % sameCounterInc == 0 && sameCounter != 0 || score > scoreInc ){
+        if (sameCounter % sameCounterInc == 0 && sameCounter != 0 || score > scoreInc) {
             scoreInc += 1000;
             sameCounterInc += 25;
             // var tot = balls.length + 10;
             leveling(10, 2);
         }
-
-
-        if( characterRadius > 1000 ){
+        if (characterRadius > 1000 ) {
             beginGame = 2;
             hasLost = true;
-        // characterRadius = 10;
+            // characterRadius = 10;
         }
-
         drawCharacter();
         break;
+
     case 2: //lose
-        if(showHow){
+        if (showHow) {
             context.fillStyle = fontColor;
             context.fillText('Collide with balls of the same color.', 50, 100);
             context.fillText('Be careful though! You don\'t want to', 50, 125);
             context.fillText('mess with the other colors. :>', 50, 150);
         }
         characterRadius = 20;
-        for( var i = 0; i < menuBalls.length; i++){
-            if( i == 0 ){
-                nameBall = 'Again?                                                    ';
-            } else if( i == 1 ){
+        for (var i = 0; i < menuBalls.length; i++) {
+            if (i == 0) {
+                nameBall = 'Again?';
+            } else if (i == 1) {
                 nameBall = 'How ';
-            } else{
+            } else {
                 nameBall = 'Lights';
             }
             menuBalls[i].update().draw(nameBall);
-            }
-
+        }
         drawCharacter();
-
-    break;
-
+        break;
     }
 
     var scoreText = 'Score: ';
@@ -371,42 +338,39 @@ function world() {
 //////////ALL EVENT LISTENERS/////////////////////////////////////////////////
 
 window.addEventListener('mousemove', function(e) {
-  mouseX = e.pageX;
-  mouseY = e.pageY;
-
-  mouseMoved = true;
+    mouseX = e.pageX;
+    mouseY = e.pageY;
+    mouseMoved = true;
 });
 
 window.addEventListener('click', function(e){
-  clickX = e.pageX;
-  clickY = e.pageY;
-
-        for(var i = 0; i < menuBalls.length; i++ ){
-        if( getHypothenuse(clickX, clickY, menuBalls[i].x, menuBalls[i].y) < menuBalls[i].radius){
-          switch( i ){
-            case 0:
-              beginGame = 1;
-              initializeGame();
-              showHow = false;
-              break;
-            case 1:
-                showHow = true;
-              break;
-            case 2:
-              if( defaultColor ){
-                defaultColor = false;
-                bckgrColor = "#2c2e30";
-                fontColor = '#fff';
-              } else{
-                defaultColor = true;
-                bckgrColor = "#fff";
-                fontColor = '#000';
-              }
-              break;
-          }
+    clickX = e.pageX;
+    clickY = e.pageY;
+    for (var i = 0; i < menuBalls.length; i++ ) {
+        if (getHypothenuse(clickX, clickY, menuBalls[i].x, menuBalls[i].y) < menuBalls[i].radius) {
+            switch( i ){
+                case 0:
+                    beginGame = 1;
+                    initializeGame();
+                    showHow = false;
+                    break;
+                case 1:
+                    showHow = true;
+                    break;
+                case 2:
+                    if (defaultColor) {
+                        defaultColor = false;
+                        bckgrColor = "#2c2e30";
+                        fontColor = '#fff';
+                    } else {
+                        defaultColor = true;
+                        bckgrColor = "#fff";
+                        fontColor = '#000';
+                    }
+                    break;
+            }
         }
     }
-
 });
 
 /////////////////////////////////////////////////////////////////////////////////////////
